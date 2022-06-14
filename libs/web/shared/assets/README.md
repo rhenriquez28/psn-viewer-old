244972cb79778fc21b4c93fe69d75deb0f62cb6e
Roy Henriquez <roy.henriquez@halopowered.com>
2022-06-14 17:30:36 -0500

    feat: se creo footer y se organizo el game card

---
+++ libs/web/shared/assets/README.md
@@ -0,0 +1,14 @@
+# web-shared-assets
+
+This library was generated with [Nx](https://nx.dev).
+
+
+## Running unit tests
+
+Run `nx test web-shared-assets` to execute the unit tests via [Jest](https://jestjs.io).
+
+
+## Running lint
+
+Run `nx lint web-shared-assets` to execute the lint via [ESLint](https://eslint.org/).
+
--- libs/web/shell/ui/layout/src/lib/layout.module.ts
+++ libs/web/shell/ui/layout/src/lib/layout.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FooterModule } from '@psn-viewer/web/shell/ui/footer';
 import { TopBarModule } from '@psn-viewer/web/shell/ui/top-bar';
 import { LayoutComponent } from './layout.component';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, TopBarModule],
+  imports: [CommonModule, RouterModule, TopBarModule, FooterModule],
   declarations: [LayoutComponent],
   exports: [LayoutComponent],
 })
